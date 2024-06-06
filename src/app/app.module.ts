d6d399bedb3e0d5a6aaa705b60bac36779f148e9
sherboo96 <sherbeny2096@gmail.com>
2024-06-06 10:58:57 +0300

    Intial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {RequestDetailsComponent} from './request-details/request-details.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RequestDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
